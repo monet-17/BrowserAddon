bb2cfb4070825ff2bc38449fa4beeb7e4b0ccd8e
Username-txt-exe <viktorivaniuk2009@gmail.com>
2024-11-24 18:21:09 +0100

    Icon, basic highlight, doesn't update when page changes though

---
+++ extensionFolder/code/script/test.js
@@ -0,0 +1,5 @@
+const allElements = document.querySelectorAll("button, input, a");
+allElements.forEach(element => {
+    element.style.border = "2px solid yellow";
+});
+//basic highlight, I wt to ks rn,
