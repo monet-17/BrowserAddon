2ae57a2ba2d4d4f02594092e71b82b2960436d6d
lemonade1717 <oleksiipervov17@gmail.com>
2024-12-08 14:20:51 +0100

    line-heightAlgorithm

--- extensionFolder/content.js
+++ extensionFolder/content.js
@@ -3,10 +3,17 @@ console.log("Content running");
 chrome.runtime.onMessage.addListener(gotMessage);
 
 function gotMessage(message, sender, sendResponse) {
-  console.log(message);
-  let paragraphs = document.getElementsByTagName('p');
-  for (elt of paragraphs) {
-    console.log(message.txt);
-    elt.style['font-size'] = `${message.txt}px`;
-  }
+	console.log(message);
+	let paragraphs = document.getElementsByTagName('p');
+	for (elt of paragraphs) {
+		console.log(message.txt);
+		elt.style['font-size'] = `${message.txt}px`;
+
+		let lineHeight = lineheightRatio(parseInt(message.txt));
+		elt.style['line-height'] = `${lineHeight}`;
+	}
+
+	function lineheightRatio(x) {
+		return (1.2 * x) / 16;
+	}
 }
