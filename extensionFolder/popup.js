62b2971d944341d743530b8063300df73a89fd1a
lemonade1717 <oleksiipervov17@gmail.com>
2025-01-12 19:46:05 +0100

    submitRemoved

--- extensionFolder/popup.js
+++ extensionFolder/popup.js
@@ -2,31 +2,31 @@ function setup() {
 	noCanvas();
 	console.log("popup running");
 
-	let submitBtn = document.getElementById("submit");
-	submitBtn.onclick = () => {
+	let sizes = select('#size');
+	let highcontrast = select('#highcontrast');
+	let monochromacy = select('#monochromacy');
+	let assistant = select('#assistant');
 
-		let sizes = select('#size');
-		let highcontrast = select('#highcontrast');
-		let monochromacy = select('#monochromacy');
-		let assistant = select('#assistant');
+	sizes.input(sendMessage);
+	highcontrast.changed(sendMessage);
+	monochromacy.changed(sendMessage);
+	assistant.changed(sendMessage);
+
+	function sendMessage() {
+		let message = {
+			fsize: sizes.value(),
+			hcontrast: highcontrast.checked(),
+			monochrome: monochromacy.checked(),
+			assist: assistant.checked()
+		};
 
 		let params = {
 			active: true,
 			currentWindow: true
-		}
+		};
 
-		chrome.tabs.query(params, gotTabs);
-
-		function gotTabs(tabs) {
-			let message = {
-				fsize: sizes.value(),
-				hcontrast: highcontrast.checked(),
-				monochrome: monochromacy.checked(),
-				assist: assistant.checked()
-			}
-			
-
+		chrome.tabs.query(params, (tabs) => {
 			chrome.tabs.sendMessage(tabs[0].id, message);
-		}
+		});
 	}
 }
